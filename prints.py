a9d4abde1d8f12f9ea7782a251dee70140ef8129
KaranGauswami <karangauswami.dev@gmail.com>
2019-02-16 12:20:26 +0530

    Removed Python 2 code

--- prints.py
+++ prints.py
@@ -2,12 +2,7 @@ from __future__ import print_function
 import platform
 Platform = platform.system()
 if Platform != 'Windows':
-    try:
-        #python
-        import __builtin__
-    except ImportError:
-        # Python 3
-        import builtins as __builtin__
+    import builtins as __builtin__
     def prints(color,*args, **kwargs):
         __builtin__.print('\033[01m',end='')
         __builtin__.print(color,end='')
