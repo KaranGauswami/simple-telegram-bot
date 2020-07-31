adeee674ea8a21ff004b2655718c7e620ee51ae1
KaranGauswami <karangauswami.dev@gmail.com>
2020-07-31 23:03:45 +0530

    added class based file

--- prints.py
+++ prints.py
@@ -1,14 +1,12 @@
 from __future__ import print_function
 import platform
 Platform = platform.system()
-if Platform != 'Windows':
-    import builtins
-    def prints(color,*args, **kwargs):
-        builtins.print('\033[01m',end='')
-        builtins.print(color,end='')
-        builtins.print(*args,**kwargs)
-        builtins.print('\033[0m',end='')
-else:
-    def prints(color,*args,**kwargs):
-        builtins.print(*args,**kwargs)
+import builtins
+def prints(color,*args, **kwargs):
+    builtins.print('\033[01m',end='')
+    builtins.print(color,end='')
+    builtins.print(*args,**kwargs)
+    builtins.print('\033[0m',end='')
 
+if __name__=='__main__':
+    print(Platform)
